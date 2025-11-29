df7b69e2aa98f627f1f6af4f45fabdc03f2a74c0
Anton Borodin <bor183@mail.ru>
2025-11-29 22:25:08 +0500

    Add Invalid Test

--- src/test/java/ru/netology/seleniumTest/SeleniumWebTest.java
+++ src/test/java/ru/netology/seleniumTest/SeleniumWebTest.java
@@ -39,7 +39,7 @@ public class SeleniumWebTest {
     }
 
     @Test
-    public void shouldOpenMainPage() {
+    public void testValidForm() {
         // тут адрес SUT, который крутится локально
         driver.get("http://localhost:9999");
         // Вводим валидные данные в поле ФИО
@@ -58,7 +58,81 @@ public class SeleniumWebTest {
                 .getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
                 result.trim());
+    }
 
+    @Test
+    public void testInvalidFormName() {
+        // тут адрес SUT, который крутится локально
+        driver.get("http://localhost:9999");
+        // Пустое поле ФИО
+        driver.findElement(By.cssSelector("[data-test-id='name'] input"))
+                .sendKeys("");
+        // Пустой номер телефона
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input"))
+                .sendKeys("");
+        // Нажимаем кнопку отправить
+        driver.findElement(By.cssSelector("button.button_view_extra"))
+                .click();
+        String result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"))
+                .getText().trim();
+        assertEquals("Поле обязательно для заполнения",
+                result.trim());
+    }
 
+    @Test
+    public void testInvalidFormPhone() {
+        // тут адрес SUT, который крутится локально
+        driver.get("http://localhost:9999");
+        // Валидное ФИО
+        driver.findElement(By.cssSelector("[data-test-id='name'] input"))
+                .sendKeys("Иванов Иван");
+        // Пустой номер телефона
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input"))
+                .sendKeys("");
+        // Нажимаем кнопку отправить
+        driver.findElement(By.cssSelector("button.button_view_extra"))
+                .click();
+        String result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"))
+                .getText().trim();
+        assertEquals("Поле обязательно для заполнения",
+                result.trim());
+    }
+
+    @Test
+    public void testInvalidFormNameEN() {
+        // тут адрес SUT, который крутится локально
+        driver.get("http://localhost:9999");
+        // Невалидное поле ФИО
+        driver.findElement(By.cssSelector("[data-test-id='name'] input"))
+                .sendKeys("Ivanov Ivan");
+        // Пустой номер телефона
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input"))
+                .sendKeys("");
+        // Нажимаем кнопку отправить
+        driver.findElement(By.cssSelector("button.button_view_extra"))
+                .click();
+        String result = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub"))
+                .getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                result.trim());
+    }
+
+    @Test
+    public void testInvalidFormPhoneStart8() {
+        // тут адрес SUT, который крутится локально
+        driver.get("http://localhost:9999");
+        // Валидное поле ФИО
+        driver.findElement(By.cssSelector("[data-test-id='name'] input"))
+                .sendKeys("Иванов Иван");
+        // Невалидный номер телефона
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input"))
+                .sendKeys("81234567890");
+        // Нажимаем кнопку отправить
+        driver.findElement(By.cssSelector("button.button_view_extra"))
+                .click();
+        String result = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub"))
+                .getText().trim();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                result.trim());
     }
 }
